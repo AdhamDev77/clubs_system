a0feed917776c2561361153ea1bd9dc8d3bc3cbf
Adham <112887020+AdhamDev77@users.noreply.github.com>
2024-05-13 01:43:52 +0300

    my changes

--- routes/clubs.js
+++ routes/clubs.js
@@ -16,7 +16,7 @@ router.get('/', getClubs);
 // Retrieve a specific club by ID
 router.get('/:id', getOneClub);
 
-router.get('/clubs/search', searchClubs)
+router.post('/clubs/search', searchClubs)
 
 // Create a new club
 router.post('/', createClub);
